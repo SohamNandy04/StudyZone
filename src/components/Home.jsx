55641d2bc329e057aec29738e3cb076e485a0a9b
Soham Nandy <sohamnandy04@gmail.com>
2024-07-19 20:59:30 +0530

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,34 +12,34 @@ function Home() {
                 <hr/>
                 <ul className="nav nav-pills flex-column mb-auto">
                 <li className="nav-item">
-                    <Link href="#" className="nav-link active" aria-current="page">
-                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#home"></use></svg>
+                    <Link href="/" className="nav-link active" aria-current="page">
+                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/home"></use></svg>
                     Home
                     </Link>
                 </li>
                 <li>
-                    <Link href="#" className="nav-link text-white">
-                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#speedometer2"></use></svg>
+                    <Link href="/" className="nav-link text-white">
+                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/speedometer2"></use></svg>
                     Courses
                     </Link>
                 </li>
                 <li>
-                    <Link href="#" className="nav-link text-white">
-                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="#table"></use></svg>
+                    <Link href="/" className="nav-link text-white">
+                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/table"></use></svg>
                     Profile
                     </Link>
                 </li>
                 </ul>
                 <hr/>
                 <div className="dropdown">
-                    <Link href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    <Link href="/" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="https://github.com/mdo.png" alt="" width="32" height="32" className="rounded-circle me-2"/>
                         <strong>mdo</strong>
                     </Link>
                     <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><Link className="dropdown-item" href="#">Settings</Link></li>
+                        <li><Link className="dropdown-item" href="/">Settings</Link></li>
                         <li><hr className="dropdown-divider"/></li>
-                        <li><Link className="dropdown-item" href="#">Sign out</Link></li>
+                        <li><Link className="dropdown-item" href="/">Sign out</Link></li>
                     </ul>
                 </div>
             </div>
