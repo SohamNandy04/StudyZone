da5b860091da7d79695b7718407bc4c2927f1af9
Soham Nandy <sohamnandy04@gmail.com>
2024-07-19 20:55:40 +0530

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ function Home() {
 
     return (
         <>
-            <div className="sidebar"><div className="d-flex flex-column flex-shrink-0 p-3 text-bg-light" style="width: 280px;" >
+            <div className="sidebar"><div className="d-flex flex-column flex-shrink-0 p-3 text-bg-light" style={'width: 280px'} >
                 <Link href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                <svg className="bi pe-none me-2" width="40" height="32"><use xlinkHref="#bootstrap"></use></svg>
+                
                 <span className="fs-4">Sidebar</span>
                 </Link>
                 <hr/>
@@ -37,9 +37,9 @@ function Home() {
                         <strong>mdo</strong>
                     </Link>
                     <ul className="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><a className="dropdown-item" href="#">Settings</a></li>
+                        <li><Link className="dropdown-item" href="#">Settings</Link></li>
                         <li><hr className="dropdown-divider"/></li>
-                        <li><a className="dropdown-item" href="#">Sign out</a></li>
+                        <li><Link className="dropdown-item" href="#">Sign out</Link></li>
                     </ul>
                 </div>
             </div>
