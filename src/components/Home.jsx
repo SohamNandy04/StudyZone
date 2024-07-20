77f5b0c720f6258ec39331abf442a32cbb553f45
Soham Nandy <sohamnandy04@gmail.com>
2024-07-20 14:55:45 +0530

    new commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,31 +4,31 @@ function Home() {
 
     return (
         <>
-            <div className="sidebar"><div className="d-flex flex-column flex-shrink-0 p-3 text-bg-light" style={'width: 280px'} >
+            <div className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark mt-50" style={{width: '280px',marginTop: '3.7rem'}} >
                 <Link href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 
                 <span className="fs-4">Sidebar</span>
                 </Link>
                 <hr/>
                 <ul className="nav nav-pills flex-column mb-auto">
-                <li className="nav-item">
-                    <Link href="/" className="nav-link active" aria-current="page">
-                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/home"></use></svg>
-                    Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/" className="nav-link text-white">
-                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/speedometer2"></use></svg>
-                    Courses
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/" className="nav-link text-white">
-                    <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/table"></use></svg>
-                    Profile
-                    </Link>
-                </li>
+                    <li className="nav-item">
+                        <Link href="/" className="nav-link active" aria-current="page">
+                        <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/home"></use></svg>
+                        Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/" className="nav-link text-white">
+                        <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/courses"></use></svg>
+                        Courses
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/" className="nav-link text-white">
+                        <svg className="bi pe-none me-2" width="16" height="16"><use xlinkHref="/profile"></use></svg>
+                        Profile
+                        </Link>
+                    </li>
                 </ul>
                 <hr/>
                 <div className="dropdown">
@@ -42,7 +42,6 @@ function Home() {
                         <li><Link className="dropdown-item" href="/">Sign out</Link></li>
                     </ul>
                 </div>
-            </div>
             </div>
         </>
     )
