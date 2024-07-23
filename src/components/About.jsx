feca1f46b1757b8cb5e527d5e502e7a67154bf22
Soham Nandy <sohamnandy04@gmail.com>
2024-07-23 12:37:51 +0530

    new commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,75 @@ function About() {
 
     return(
         <>
-            <div className="context">
-                <p className="text-start lh-sm">Confused on which course to take? We have got you covered.</p>
-                <p className="text-start lh-sm">Browse courses and find out the best course for you.</p>
-                <p className="text-start lh-sm">Its free! StudyZone is going to teach basics and those coding techniques to people in short time which can take ages to learn.</p>
+            <div>
+                <div className="context">
+                    <p className="text-start lh-sm">Confused on which course to take? We have got you covered.</p>
+                    <p className="text-start lh-sm">Browse courses and find out the best course for you.</p>
+                    <p className="text-start lh-sm">Its free! StudyZone is going to teach basics and those coding techniques to people in short time which can take ages to learn.</p>
+                </div>
+
+                <div className="accordion" id="accordionPanelsStayOpenExample">
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                            <strong>Why should be take courses on StudyZone ?</strong>
+                        </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show">
+                            <div className="accordion-body">
+                                It is shown by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It&apos;s also worth noting that just about any HTML can go within the, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                            <strong>Accordion Item #2</strong>
+                        </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseTwo" className="accordion-collapse collapse">
+                            <div className="accordion-body">
+                                It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It&apos;s also worth noting that just about any HTML can go within the, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                            <strong>Accordion Item #3</strong>
+                        </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseThree" className="accordion-collapse collapse">
+                            <div className="accordion-body">
+                                It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It&apos;s also worth noting that just about any HTML can go within the, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour" aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
+                            <strong>Accordion Item #4</strong>
+                        </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseFour" className="accordion-collapse collapse">
+                            <div className="accordion-body">
+                                It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It&apos;s also worth noting that just about any HTML can go within the, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="accordion-item">
+                        <h2 className="accordion-header">
+                        <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive" aria-expanded="false" aria-controls="panelsStayOpen-collapseFive">
+                            <strong>Accordion Item #5</strong>
+                        </button>
+                        </h2>
+                        <div id="panelsStayOpen-collapseFive" className="accordion-collapse collapse">
+                            <div className="accordion-body">
+                                It is hidden by default, until the collapse plugin adds the appropriate classNamees that we use to style each element. These classNamees control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It&apos;s also worth noting that just about any HTML can go within the, though the transition does limit overflow.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
         <footer className="py-2">
             <ul className="nav justify-content-center border-bottom pb-3 mb-3 fw-semibold">
                 <li className="nav-item"><a href="/home" className="litxt nav-link px-2">Home</a></li>
+                <li className="nav-item"><a href="/courses" className="litxt nav-link px-2">Courses</a></li>
                 <li className="nav-item"><a href="#" className="litxt nav-link px-2">Pricing</a></li>
-                <li className="nav-item"><a href="#" className="litxt nav-link px-2">FAQs</a></li>
                 <li className="nav-item"><a href="/about" className="litxt nav-link px-2">About</a></li>
             </ul>
-            <p className="copyrighttxt fw-semibold">Copyright © {currDate.getFullYear()} StudyZone</p>
+            <p className="copyrighttxt fw-semibold">Copyright © {currDate.getFullYear()} <a href="/" className="link-body-emphasis text-decoration-none">StudyZone</a></p>
         </footer>
     );
 }
