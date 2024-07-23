17363ea46b51e6a8c4a6b47b3e6582418c537270
Soham Nandy <sohamnandy04@gmail.com>
2024-07-23 11:55:58 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,13 @@
+import '../componentCss/About.css'
+
 function About() {
 
     return(
         <>
-            <div  className="d-flex justify-center">
-                <h1>Hello, This is About Page</h1>
-                <p>lor</p>
+            <div className="context">
+                <p className="text-start lh-sm">Confused on which course to take? We have got you covered.</p>
+                <p className="text-start lh-sm">Browse courses and find out the best course for you.</p>
+                <p className="text-start lh-sm">Its free! StudyZone is going to teach basics and those coding techniques to people in short time which can take ages to learn.</p>
             </div>
         </>
     )
