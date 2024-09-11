eba375149dd49e93ae473a8d4ce14019a16a6446
Soham Nandy <sohamnandy04@gmail.com>
2024-09-11 17:13:32 +0530

    new commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,53 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import { NavLink } from "react-router-dom";
 import "../componentCss/Login.css"
 
 function Login() {
+
+    const [email,setEmail] = useState()
+    const [password,setPassword] = useState()
+
+    const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        axios.post("api/v1/users/login",{email,password})
+        .then(result => {
+            console.log(result)
+            if(result.data === "Success"){
+                navigate("/home")
+            }else{
+                navigate("/signup")
+                alert("You're not registered")
+            }
+        }).catch(err => console.log(err))
+    }
+
+
     return (
         <div className="login-page d-flex justify-content-center align-items-center vh-100">
             <div className="login-container p-4 p-md-5 rounded bg-light shadow">
                 <h3 className="text-center mb-4">Sign In</h3>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email ID</label>
-                        <input type="email" id="email" placeholder="Enter Email" className="form-control" autoComplete="off" />
+                        <input type="email" id="email" placeholder="Enter Email" className="form-control" autoComplete="off" required 
+                        onChange = {(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" id="password" placeholder="Enter Password" className="form-control" />
+                        <input type="password" id="password" placeholder="Enter Password" className="form-control" required
+                        onChange = {(e) => setPassword(e.target.value)}/>
                     </div>
                     <div className="mb-3 form-check">
                         <input type="checkbox" className="form-check-input" id="check" />
                         <label htmlFor="check" className="form-check-label">Remember Me</label>
                     </div>
                     <div className="d-grid">
-                        <button type="button" className="btn btn-primary">Sign In</button>
+                        <button type="submit" className="btn btn-primary">Sign In</button>
                     </div>
                     <div className="text-center mt-3">
                         <p>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,49 +1,55 @@
-// import { useState } from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 // import regispic from '../assets/footerimg.png'
 
 import { NavLink } from "react-router-dom"
 
 function Signup(){
 
-    // const [user, setUser] = useState({
-    //     username: "",
-    // });
+    const [name, setName] = useState()
+    const [phone, setPhone] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const navigate = useNavigate()
 
-    // const handleInput = (e) => {
-    //     let name = e.target.name;
-    //     let value = e.target.value;
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        
+        axios.post("api/v1/users/register",{name,phone,email,password})
+        .then(result => {console.log(result)
+            navigate("/login")}).catch(err => console.log(err))
+    }
 
-    //     setUser({
-    //         ...user,
-    //     [name] : value,
-    //     })
-    // }
-    // value={user.username}
-    // onChange={handleInput}
 
     return (
         <div className="signup-page d-flex justify-content-center align-items-center vh-100" style={{marginTop: '50px'}}>
             <div className="signup-container p-4 p-md-5 rounded bg-light shadow">
                 <h3 className="text-center mb-4">Sign Up</h3>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="fname" className="form-label">First Name</label>
-                        <input type="text" id="fname" placeholder="Enter First Name" className="form-control" autoComplete="off" required />
+                        <input type="text" id="fname" placeholder="Enter First Name" className="form-control" autoComplete="off" required 
+                        onChange={(e) => setName(e.target.value)}
+                        />
                     </div>
                     <div className="mb-3">
                         <label htmlFor="lname" className="form-label">Phone Number</label>
-                        <input type="text" id="phno" placeholder="Enter Phone Number" className="form-control" autoComplete="off" required />
+                        <input type="text" id="phno" placeholder="Enter Phone Number" className="form-control" autoComplete="off" required 
+                        onChange={(e) => setPhone(e.target.value)}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email ID</label>
-                        <input type="email" id="email" placeholder="Enter Email" className="form-control" autoComplete="off" required/>
+                        <input type="email" id="email" placeholder="Enter Email" className="form-control" autoComplete="off" required
+                        onChange={(e) => setEmail(e.target.value)}/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <input type="password" id="password" placeholder="Enter Password" className="form-control" required />
+                        <input type="password" id="password" placeholder="Enter Password" className="form-control" required 
+                        onChange={(e) => setPassword(e.target.value)}/>
                     </div>
                     <div className="d-grid">
-                        <button type="button" className="btn btn-primary">Register</button>
+                        <button type="submit" className="btn btn-primary">Register</button>
                     </div>
                     <div className="text-center mt-3">
                         <p>
